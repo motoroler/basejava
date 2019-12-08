0026c386d7e97b7e8f038e22704589664b3e9d03
motoroler <cyclopin@yandex.ru>
2019-12-08 16:33:10 +0300

    Fixed all code review's findings

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -10,43 +10,39 @@ import java.util.Arrays;
 public class ArrayStorage {
     private final int ARRAY_SIZE = 10_000;
     private Resume[] storage = new Resume[ARRAY_SIZE];
-    private int nextFreeElement = 0;
+    private int size = 0;
 
     public void clear() {
-        Arrays.fill(storage, 0, nextFreeElement, null);
+        Arrays.fill(storage, 0, size, null);
         System.out.println("The storage was cleared");
-        nextFreeElement = 0;
+        size = 0;
     }
 
-    public void save(Resume r) {
-        int index = -1;
-        if (0 <= isExist(r.getUuid())) {
-            System.out.printf("ERROR: You try to add CV %s, which already existed in the storage\n", r);
+    public void save(Resume resume) {
+        if (0 <= getIndex(resume.getUuid())) {
+            System.out.printf("ERROR: You try to add CV %s, which already existed in the storage\n", resume);
+        } else if (size < ARRAY_SIZE) {
+                storage[size++] = resume;
+                System.out.printf("New resume %s was added to the storage\n", resume);
         } else {
-            if (nextFreeElement < ARRAY_SIZE) {
-                storage[nextFreeElement++] = r;
-                System.out.printf("New resume %s was added to the storage\n", r);
-            } else {
-                System.out.println("WARNING! Storage is full!");
-            }
+            System.out.println("WARNING! Storage is full!");
         }
     }
 
     public Resume get(String uuid) {
-        int index = -1;
-        if (0 <= (index = isExist(uuid))) {
+        int index = getIndex(uuid);
+        if (0 <= index) {
             return storage[index];
-        } else {
-            System.out.printf("Resume %s was not found\n", uuid);
-            return null;
         }
+        System.out.printf("Resume %s was not found\n", uuid);
+        return null;
     }
 
     public void delete(String uuid) {
-        int index = -1;
-        if (0 <= (index = isExist(uuid))) {
-            storage[index] = storage[nextFreeElement - 1];
-            storage[nextFreeElement-- -1] = null;
+        int index = getIndex(uuid);
+        if (0 <= index) {
+            storage[index] = storage[size - 1];
+            storage[size-- -1] = null;
             System.out.printf( "%s resume was deleted\n", uuid);
         } else {
             System.out.println("ERROR: Provided CV doesn't exist in the storage");
@@ -54,9 +50,9 @@ public class ArrayStorage {
     }
 
     public void update(Resume resume) {
-        int index = -1;
-        if (0 <= (index = isExist(resume.getUuid()))) {
-            //do nothing.TODO: storage[index] = resume;
+        int index = getIndex(resume.getUuid());
+        if (0 <= index) {
+            storage[index] = resume;
         }
     }
 
@@ -64,8 +60,8 @@ public class ArrayStorage {
      * @param uuid
      * @return index of uuid in the storage, if it exists in the storage. Otherwise -1 will be returned
      */
-    private int isExist(String uuid) {
-        for (int i = 0; i < nextFreeElement; i++) {
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].toString().equals(uuid)) {
                 return i;
             }
@@ -76,10 +72,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        return Arrays.copyOf(storage, nextFreeElement);
+        return Arrays.copyOf(storage, size);
     }
 
     public int size() {
-        return nextFreeElement;
+        return size;
     }
 }
